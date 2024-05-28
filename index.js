dabbdc77d815bc111e7f5c974d7278553f6ca347
Sanjay Areteminds <sanjay.murugan@areteminds.com>
2024-05-28 23:27:17 +0530

    socket added

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const http = require("http");
 const app = require("./app");
 const { Server } = require('socket.io');
 const attachIO = require('./middleware/attachIo');
+const models = require("./models")
 
 const server = http.createServer(app);
 
-const io = new Server(server);
-
+// const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 app.use(attachIO(io)); // Attach IO instance to request
 
-io.on("connection", (socket) => {
+io.on("connection",async (socket) => {
   console.log("user connected");
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
+  const prevMsg = await models.Message.findAll({ order: [['createdAt', 'ASC']] })
+
+  socket.emit('previous messages', prevMsg);
   socket.on("chat message", (message) => {
     console.log("message", message);
     socket.emit("chat message", message);
